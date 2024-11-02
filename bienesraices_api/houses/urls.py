388df45e0d01e078144c5e48d4728b4055f07c60
xJDvmPWSH <juandiegovaldesm03@gmail.com>
2024-11-02 01:23:29 -0400

    Initialize Django project structure and add Houses app with models, serializers, views, and URLs

---
+++ bienesraices_api/houses/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
+from rest_framework import routers
+from houses import views
+
+router = routers.DefaultRouter()
+router.register(r'houses', views.HouseViewSet, 'houses')
+router.register(r'sellers', views.SellerViewSet, 'sellers')
+router.register(r'houses_sellers', views.HouseSellerViewSet, 'houses_sellers')
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+    path('docs/', include_docs_urls(title='Bienes Raices API'))
+]
